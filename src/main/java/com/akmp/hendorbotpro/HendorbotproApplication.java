cbd37226aded0c7b7bf152bf27014b90091ccd41
Rainer Regan <rainercoc@gmail.com>
2021-10-05 13:59:40 +0700

    First Commit

---
+++ src/main/java/com/akmp/hendorbotpro/HendorbotproApplication.java
@@ -0,0 +1,13 @@
+package com.akmp.hendorbotpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HendorbotproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HendorbotproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akmp/hendorbotpro/HendorbotproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akmp.hendorbotpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HendorbotproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
